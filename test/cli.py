01fe625fdce8cd4e4ad37981dfe1b74205a45bba
Jeremy Fowers <jeremy.fowers@amd.com>
2024-01-10 10:38:46 -0500

    print info on test fail

--- test/cli.py
+++ test/cli.py
@@ -1004,8 +1004,12 @@ class Testing(unittest.TestCase):
         for result in result_dict.values():
             # All of the models should have exported to ONNX
             assert "export_pytorch" in result["selected_sequence_of_stages"]
-            assert result["stage_status:export_pytorch"] == "SUCCESSFUL"
-            assert result["stage_duration:export_pytorch"] > 0
+            assert result["stage_status:export_pytorch"] == "SUCCESSFUL", [
+                "stage_status:export_pytorch"
+            ]
+            assert result["stage_duration:export_pytorch"] > 0, [
+                "stage_duration:export_pytorch"
+            ]
 
 
 if __name__ == "__main__":
