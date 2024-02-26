135b08b88a8575bdc89db20285c6366adcf21a70
Jeremy Fowers <80718789+jeremyfowers@users.noreply.github.com>
2024-02-26 13:29:04 -0500

    Fix a typo in cli.py
    Signed-off-by: Jeremy Fowers <80718789+jeremyfowers@users.noreply.github.com>

--- src/turnkeyml/cli/cli.py
+++ src/turnkeyml/cli/cli.py
@@ -181,7 +181,7 @@ def main():
         dest="runtime",
         help="Software runtime that will be used to collect the benchmark. "
         "Must be compatible with the selected device. "
-        "Automatically selects a sequence if `--sequence` is not used."
+        "Automatically selects a sequence if `--sequence` is not used. "
         "If this argument is not set, the default runtime of the selected device will be used.",
         required=False,
         default=None,
