03d5dd95105553c7adfc622a506c9c54729f0eba
Jeremy Fowers <jeremy.fowers@amd.com>
2024-07-16 13:07:49 -0400

    Fixes from Krishna's feedback

--- models/torch_hub/skip/efficientnet_b0.py
+++ models/torch_hub/skip/efficientnet_b0.py
@@ -1,5 +1,5 @@
 # labels: test_group::turnkey name::efficientnet_b0 author::torch_hub task::Computer_Vision license::bsd-3-clause
-# Skip reason: Fails during the discover tool of turnkey
+# Skip reason: Fails during the use of the Discover Tool in turnkey
 from turnkeyml.parser import parse
 import torch
 from torchvision.models import EfficientNet_B0_Weights
