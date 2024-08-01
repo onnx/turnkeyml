51720b55b52b199913c50897885a570229337407
Jeremy Fowers <jeremy.fowers@amd.com>
2024-08-01 13:01:07 -0400

    update plugins python version

--- examples/cli/plugins/example_rt/setup.py
+++ examples/cli/plugins/example_rt/setup.py
@@ -4,5 +4,5 @@ setup(
     name="turnkeyml_plugin_example_rt",
     version="0.0.0",
     packages=["turnkeyml_plugin_example_rt"],
-    python_requires=">=3.8, <3.11",
+    python_requires=">=3.8, <3.12",
 )
--- examples/cli/plugins/example_tool/setup.py
+++ examples/cli/plugins/example_tool/setup.py
@@ -4,5 +4,5 @@ setup(
     name="turnkeyml_plugin_example_tool",
     version="0.0.0",
     packages=["turnkeyml_plugin_example_tool"],
-    python_requires=">=3.8, <3.11",
+    python_requires=">=3.8, <3.12",
 )
