c6039acbee846bec757142589e9d349ed439256f
Jeremy Fowers <80718789+jeremyfowers@users.noreply.github.com>
2024-08-08 08:22:33 -0400

    Create examples for the new way of using the api (#211)

---
+++ examples/api/loading_a_build.py
@@ -0,0 +1,68 @@
+"""
+This example shows how to use the LoadBuild tool as an API for working
+with builds you have saved to your cache.
+
+As a prerequisite, first run the onnx_opset.py API example to make sure
+that you have a build ready for loading.
+"""
+
+import os
+import numpy as np
+import onnxruntime as ort
+from turnkeyml.common.filesystem import get_available_builds, DEFAULT_CACHE_DIR
+from turnkeyml.state import State
+from turnkeyml.tools.load_build import LoadBuild
+from turnkeyml.tools.onnx import ConvertOnnxToFp16
+
+
+def main():
+    # This is the build name specified in the previous example
+    prerequisite_build = "onnx_opset_example"
+
+    # We use the _state.yaml file in the build directory when loading a build
+    prior_state_file = os.path.join(
+        DEFAULT_CACHE_DIR,
+        prerequisite_build,
+        f"{prerequisite_build}_state.yaml",
+    )
+
+    # Make sure the "onnx_opset_example" build from the last
+    # example was actually created
+    builds = get_available_builds(DEFAULT_CACHE_DIR)
+    print("All builds available in the cache:", builds)
+
+    if prerequisite_build in builds:
+        print(f"{prerequisite_build} exists!")
+    else:
+        raise Exception(
+            f"{prerequisite_build} not found in cache. Make sure to "
+            "run the onnx_opset.py API example before this example."
+        )
+
+    # Create a placeholder State instance that we will later populate by loading the build
+    state = State(cache_dir=DEFAULT_CACHE_DIR, build_name=prerequisite_build)
+
+    # Load the prior build state into our State instance
+    # Set a skip_policy of "none" so that this example will work if you run
+    # it multiple times
+    state = LoadBuild().run(state, input=prior_state_file, skip_policy="none")
+
+    # Convert the model to fp16 using turnkey's onnx conversion tool
+    state = ConvertOnnxToFp16().run(state)
+
+    # Load the ONNX model as an InferenceSession and execute one inference
+    sess_options = ort.SessionOptions()
+    sess_options.graph_optimization_level = ort.GraphOptimizationLevel.ORT_ENABLE_ALL
+    onnx_session = ort.InferenceSession(state.results, sess_options)
+
+    # x is the example input tensor from the onnx_opset_example
+    input_size = 9
+    x = np.random.rand(input_size).astype(np.float16)
+
+    # Run inference
+    out = onnx_session.run([onnx_session.get_outputs()[0].name], {"x": x})
+    print("Output:", out[0])
+
+
+if __name__ == "__main__":
+    main()
--- examples/api/onnx_opset.py
+++ examples/api/onnx_opset.py
@@ -1,17 +1,21 @@
 """
-Example script that demonstrates how to set a custom ONNX opset for a build run
+This example shows how to call tools as their own standalone API.
+
+Every Tool class has a run() method that can be used to programmatically
+define a build. This is helpful in scenarios where the turnkey CLI is not
+flexible enough to run the build(s) you want in the way that you want.
+
+In this specific example, we will call the tools as APIs and set a custom ONNX opset.
 
 You can run this script in your turnkey Conda environment with:
     python onnx_opset.py --onnx-opset YOUR_OPSET
-
-And then you can observe the ONNX opset in the resulting ONNX files with:
-   turnkey cache --stats -b BUILD_NAME
 """
 
 import pathlib
 import argparse
-from turnkeyml import evaluate_files
-from turnkeyml.sequence import Sequence
+import os
+from turnkeyml.state import State
+from turnkeyml.common.filesystem import Stats, DEFAULT_CACHE_DIR
 from turnkeyml.tools.export import ExportPytorchModel
 from turnkeyml.tools.discovery import Discover
 
@@ -26,6 +30,7 @@ def main():
     parser.add_argument(
         "--onnx-opset",
         default="16",
+        type=int,
         help="ONNX opset to use when creating ONNX files",
     )
 
@@ -36,13 +41,29 @@ def main():
         pathlib.Path(__file__).parent.resolve() / "scripts" / "hello_world.py"
     )
 
-    sequence = Sequence(
-        tools={Discover(): [], ExportPytorchModel(): ["--opset", args.onnx_opset]}
-    )
-    evaluate_files(
-        input_files=[path_to_hello_world_script],
-        sequence=sequence,
-    )
+    # Create a State instance
+    # The arguments we pass here let the tools know where to store
+    # logs and results on disk
+    state = State(cache_dir=DEFAULT_CACHE_DIR, build_name="onnx_opset_example")
+
+    # Manually run the Discover tool as an API
+    # This will populate state.result with a PyTorch model instance
+    # extracted from the target script
+    state = Discover().run(state, input=path_to_hello_world_script)
+
+    # Manually run the ExportPytorchModel tool as an API
+    # This will export the PyTorch model in state.result into an ONNX file
+    state = ExportPytorchModel().run(state, opset=args.onnx_opset)
+
+    # Save the build state to make sure we can access this build later
+    state.save()
+
+    # See what we learned about the model during discovery and export
+    stats = Stats(state.cache_dir, state.build_name).stats
+    print("Parameters:", stats["parameters"])
+
+    # Make sure the ONNX file really exists
+    print("Build result file:", state.results, os.path.exists(state.results))
 
 
 if __name__ == "__main__":
