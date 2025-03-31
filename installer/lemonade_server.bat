c81c88fcb6802e5207d05f13df2b1b57d991d11c
danielholanda <holand.daniel@gmail.com>
2025-03-31 12:18:17 -0700

    Release 6.1.2

---
+++ installer/lemonade_server.bat
@@ -0,0 +1,33 @@
+@echo off
+setlocal enabledelayedexpansion
+set CONDA_ENV=lemon_env
+
+REM --keep-alive is only used by the bash script to make sure that, if the server fails to open, we don't close the terminal right away.
+REM Check for --keep-alive argument and remove it from arguments passed to CLI
+set KEEP_ALIVE=0
+set ARGS=
+for %%a in (%*) do (
+    if /I "%%a"=="--keep-alive" (
+        set KEEP_ALIVE=1
+    ) else (
+        set ARGS=!ARGS! %%a
+    )
+)
+
+REM Change to parent directory where conda env and bin folders are located
+pushd "%~dp0.."
+
+REM Run the Python CLI script through conda, passing filtered arguments
+call conda run --no-capture-output -p "%CD%\%CONDA_ENV%" lemonade-server-dev !ARGS!
+popd
+
+REM Error handling: Show message and pause if --keep-alive was specified
+if %ERRORLEVEL% neq 0 (
+    if %KEEP_ALIVE%==1 (
+        echo.
+        echo An error occurred while running Lemonade Server.
+        echo Please check the error message above.
+        echo.
+        pause
+    )
+)
